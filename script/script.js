a481c49f885ffd95d3855d7229818f5d59f08498
sueng0422 <sueng0422@naver.com>
2022-11-19 01:55:09 +0900

    commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,17 @@
 (function(){
-    
+    var slideCount = $(".page6slideimg").lenth;
+    var slideIndex = 0;
+    var slidePosition;
+
+    setInterval(function(){
+        if(slideIndex < slideCount-1){
+            slideIndex++;
+        }else{
+            slideIndex = 0;
+        }
+
+        slidePosition = slideIndex * (-634.31)+"px";
+        $(".page6imgList").animate({left:slidePosition},400);
+    })
+    console.log("test");
 })(jQuery);
