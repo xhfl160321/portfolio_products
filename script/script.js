fd359105c363b8a0bb446559eafafc5b93d63680
sueng0422 <sueng0422@naver.com>
2023-09-18 23:58:08 +0900

    commit jq

--- script/script.js
+++ script/script.js
@@ -13,5 +13,14 @@
         slidePosition = slideIndex * (-634.31)+"px";
         $(".page6imgList").animate({left:slidePosition},400);
     })
-    console.log("test");
+    // console.log("test");
+
+    //클릭 시 부드러운 scroll down
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
 })(jQuery);
